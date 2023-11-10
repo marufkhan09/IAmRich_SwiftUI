b45779b5bde300fff2f6befd2c313b9c8497d9b2
marufkhan09 <maruf1609@gmail.com>
2023-11-11 03:52:50 +0600

    implemented simple ui with swift ui

--- IAmRichSwiftUI/ContentView.swift
+++ IAmRichSwiftUI/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(content: {
+            Color(.systemTeal).ignoresSafeArea()
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40)).multilineTextAlignment(.center).bold()
+                let url = URL(string: "https://images.unsplash.com/photo-1682686580433-2af05ee670ad?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+            
+                Image("diamond").resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 200)
+            }
+            .padding()
+        })
     }
 }
 
