c4218b634bbd7b1345127fbb3bd6f4cf6d26939d
marufkhan09 <maruf1609@gmail.com>
2023-11-11 03:33:08 +0600

    Initial Commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Maruf Khan on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
